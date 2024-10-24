04e5953e507a5532979f2039ab882ff9e95fda7e
w92704dw <dylan.wilks@student.manchester.ac.uk>
2024-10-24 01:18:51 +0100

    Added macro for unbound items.

--- src/items_macro.rs
+++ src/items_macro.rs
@@ -16,7 +16,7 @@ macro_rules! items {
             ProblemItems::<$item_type, 
                            $length, 
                            $item_type
-                          >::new();
+                           >::new();
             $(
                 $items_name.add($crate::item::
                 Item::<$item_type, 
@@ -30,8 +30,47 @@ macro_rules! items {
                                     $($quantity,)?  
                                     <$item_type as $crate::
                                           compatible_problem_type_trait::
-                                          CompatibleProblemType>::
-                                          default_quantity()
+                                          CompatibleProblemType
+                                     >::default_quantity()
+                              ),
+                
+                });
+            )*
+        };
+    (@items_quantity, $quantity:expr $(, $_default:expr)?) => {
+        $quantity
+    };
+
+    (
+    $items_name:ident 
+    <
+        $item_type:ty,
+        $length:literal,
+        unbound
+    >:
+    $(
+        $val:expr,
+        [$($weights:expr),+]
+        $(, $quantity:expr)?;
+    )*
+    ) => {
+            let mut $items_name = $crate::item::
+            ProblemItems::<$item_type, 
+                           $length, 
+                           unbound
+                           >::new();
+            $(
+                $items_name.add($crate::item::
+                Item::<$item_type, 
+                       $length,
+                       unbound
+                       > {
+                    value: $val,
+                    weights: [$($weights),*],
+                    quantity: $crate::items!(
+                                    @items_quantity, 
+                                    $($quantity,)?  
+                                    unbound
                               ),
                 
                 });
